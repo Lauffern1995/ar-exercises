3fad9e176fd7e083ea1e6b7a7cf8916849c18f91
Noah <lauffern@gmail.com>
2022-04-04 18:01:39 +0000

    Ex. 5 completed

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@all = Store.sum(:annual_revenue)
+
+puts "TOTAL REV: #{@all}"
+
+@sum  = @all / Store.count
+
+puts "AVG REV: #{@sum}"
+
+@top_earners = Store.where("annual_revenue > ?", 1000000).count
+
+puts "STORES DOING OVER $1M: #{@top_earners}"
